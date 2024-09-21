77b0da8f095e5e3ae536aa3de7c9d8b412232fec
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-09-21 07:53:02 -0700

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -14,9 +14,15 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/crowdsource/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {
+        "item_id": item_id, 
+        "stateorprovince": stateorprovince, 
+        "city": city, 
+        "latitude": latitude, 
+        "longitude": longitude
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=PORT)
