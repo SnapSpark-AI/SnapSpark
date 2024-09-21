a8b50962bdc4c2ced543ace965a387cdea79bf58
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-09-21 07:44:45 -0700

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 import sys
 import os
 
 # NOTE: Always store API keys as environment variables
+
+PORT = int(sys.argv[1])
 
 app = FastAPI()
 
@@ -14,3 +17,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=PORT)
