e85a81a0a1875d8aca80286e0560715f7e6ce613
shuban-789 <cybergeek11929@gmail.com>
2024-09-21 20:23:48 -0700

    server.py

--- Backend/app.py
+++ Backend/app.py
@@ -3,13 +3,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import mysql.connector
-import urllib
 import sys
 import os
 import shutil
 import subprocess
-import PIL.Image
-import PIL.ExifTags
+from PIL import Image, ExifTags
 
 # NOTE: Always store API keys as environment variables
 PORT = int(sys.argv[1])
@@ -17,14 +15,9 @@ PORT = int(sys.argv[1])
 origins = ["http://localhost:3000"]
 
 app = FastAPI()
-mongo_uri = "mongodb://" + os.getenv("MONGO_USR") + ":" + urllib.parse.quote(os.getenv("MONGO_PASS")) + "@127.0.0.1:27001/"
 
 weather_api = os.getenv("WEATHER_API_DEFAULT")
 
-client = AsyncIOMotorClient(mongo_uri)
-db = client.myDatabase
-print(client.list_database_names())
-print(db.list_collection_names())
 UPLOAD_DIR = "uploaded_images"
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
@@ -50,15 +43,21 @@ async def upload_image(
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
-    imagename = "uploaded_images/"+file.filename
-    img = PIL.Image.open(imagename)
-    exif = {
-        PIL.ExifTags.TAGS[k]: v
-        for k, v in img._getexif().items
-        if k in PIL.ExifTags.TAGS
-    }
-    north = exif['GPSinfo'][2]
-    east = exif['GPSinfo'][4]
+    imagename = "/home/shubs/hackathon/SnapSpark/Backend/uploaded_images/" + file.filename
+    img = Image.open(imagename)
+
+    exif = img._getexif()
+
+    if exif is not None:
+        exif_data = {
+            ExifTags.TAGS[k]: v
+            for k, v in exif.items()
+            if k in ExifTags.TAGS
+        }
+    else:
+        exif_data = {}
+    north = exif_data['GPSinfo'][2]
+    east = exif_data['GPSinfo'][4]
     latitude = str((((north[0]*60)+north[1]*60)+north[2])/60/60)
     longitude = str((((east[0]*60)+east[1]*60)+east[2])/60/60)
     os.chdir("uploaded_images")
@@ -87,14 +86,6 @@ async def upload_image(
     sql = "INSERT INTO coordinates (filename, latitude, longitude) VALUES (%s, %s, %s)"
     val = (file.filename, latitude, longitude)
     mycursor.execute(sql, val)
-
-@app.get("/imagesources/{imagename}")
-async def read_image_source(imagename: str):
-    doc = await db.imagesources.find_one({"imagename": imagename})
-    if doc:
-        return doc
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=PORT)
--- Frontend/server.go
+++ Frontend/server.go
@@ -12,12 +12,12 @@ func loggingMiddleware(next http.Handler) http.Handler {
 
         rw := &responseWriter{w, http.StatusOK}
 
-        log.Printf("Received request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
-        log.Printf("Request Headers: %+v", r.Header)
+        log.Printf("[🟢] Received request: %s %s from %s", r.Method, r.URL.Path, r.RemoteAddr)
+        log.Printf("[🟢] Request Headers: %+v", r.Header)
 
         next.ServeHTTP(rw, r)
 
-        log.Printf("Responded with status: %d in %v", rw.statusCode, time.Since(start))
+        log.Printf("[🟢] Responded with status: %d in %v", rw.statusCode, time.Since(start))
     })
 }
 
@@ -35,7 +35,7 @@ func main() {
     fs := http.FileServer(http.Dir("."))
     http.Handle("/", loggingMiddleware(fs))
 
-    log.Println("Serving on http://localhost:3000")
+    log.Println("[🟢] Serving on http://localhost:3000")
     err := http.ListenAndServe(":3000", nil)
     if err != nil {
         log.Fatal(err)
