3a22a49ed236541a0b0db9c2150d7bed2f60188c
shuban-789 <cybergeek11929@gmail.com>
2024-09-21 17:48:06 -0700

    app py commit

--- Backend/app.py
+++ Backend/app.py
@@ -49,10 +49,30 @@ async def upload_image(
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     
-    result = await db.imagesources.insert_one(mongodb_document)
+    imagename = file.filename
+    img = PIL.Image.open(imagename)
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items
+        if k in PIL.ExifTags.TAGS
+    }
+    north = exif['GPSinfo'][2]
+    east = exif['GPSinfo'][4]
+    latitude = (((north[0]*60)+north[1]*60)+north[2])/60/60
+    longitude = (((east[0]*60)+east[1]*60)+east[2])/60/60
+    os.chdir("uploaded_images")
+    file_location = subprocess.run(['pwd'], capture_output=True, text=True)
+    file_location = file_location.stdout
+    mongo_store = {
+        "imagename": imagename,
+        "latitude": latitude,
+        "longitude": longitude,
+        "file_location": file_location
+    }
+    result = await db.imagesources.insert_one(mongo_store)
     
     if result.acknowledged:
-        return {"message": "Image and metadata added successfully", "item_id": item_id}
+        return {"message": "Image and metadata added successfully"}
     else:
         raise HTTPException(status_code=500, detail="Failed to insert document")
 
