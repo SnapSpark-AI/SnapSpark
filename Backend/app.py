86bff8f0225009b98f2db0204f9240e51a6527a6
shuban-789 <cybergeek11929@gmail.com>
2024-09-21 20:55:08 -0700

    mysql database is working

--- Backend/app.py
+++ Backend/app.py
@@ -8,6 +8,7 @@ import os
 import shutil
 import subprocess
 from PIL import Image, ExifTags
+from PIL.ExifTags import TAGS, GPSTAGS
 
 # NOTE: Always store API keys as environment variables
 PORT = int(sys.argv[1])
@@ -21,6 +22,46 @@ weather_api = os.getenv("WEATHER_API_DEFAULT")
 UPLOAD_DIR = "uploaded_images"
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
+
+def get_lat_long(image_path):
+    def _get_if_exist(data, key):
+        return data[key] if key in data else None
+    
+    def _convert_to_degrees(value):
+        d = float(value[0])
+        m = float(value[1])
+        s = float(value[2])
+        return d + (m / 60.0) + (s / 3600.0)
+
+    image = Image.open(image_path)
+    exif_data = image._getexif()
+    
+    if not exif_data:
+        raise ValueError("No EXIF metadata found")
+    gps_info = {}
+    for tag, value in exif_data.items():
+        tag_name = TAGS.get(tag, tag)
+        if tag_name == "GPSInfo":
+            for gps_tag in value:
+                gps_info[GPSTAGS.get(gps_tag, gps_tag)] = value[gps_tag]
+    
+    gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
+    gps_latitude_ref = _get_if_exist(gps_info, "GPSLatitudeRef")
+    gps_longitude = _get_if_exist(gps_info, "GPSLongitude")
+    gps_longitude_ref = _get_if_exist(gps_info, "GPSLongitudeRef")
+    
+    if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
+        latitude = _convert_to_degrees(gps_latitude)
+        longitude = _convert_to_degrees(gps_longitude)
+        
+        if gps_latitude_ref != "N":
+            latitude = -latitude
+        if gps_longitude_ref != "E":
+            longitude = -longitude
+        
+        return str(latitude), str(longitude)
+    else:
+        raise ValueError("GPS data not found")
 
 app.add_middleware(
     CORSMiddleware,
@@ -44,22 +85,7 @@ async def upload_image(
         shutil.copyfileobj(file.file, buffer)
     
     imagename = "/home/shubs/hackathon/SnapSpark/Backend/uploaded_images/" + file.filename
-    img = Image.open(imagename)
-
-    exif = img._getexif()
-
-    if exif is not None:
-        exif_data = {
-            ExifTags.TAGS[k]: v
-            for k, v in exif.items()
-            if k in ExifTags.TAGS
-        }
-    else:
-        exif_data = {}
-    north = exif_data['GPSinfo'][2]
-    east = exif_data['GPSinfo'][4]
-    latitude = str((((north[0]*60)+north[1]*60)+north[2])/60/60)
-    longitude = str((((east[0]*60)+east[1]*60)+east[2])/60/60)
+    latitude, longitude = get_lat_long(imagename)
     os.chdir("uploaded_images")
     file_location = subprocess.run(['pwd'], capture_output=True, text=True)
     file_location = file_location.stdout
