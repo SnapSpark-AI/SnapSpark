be8742b3880d64f931a0c91117ad92b8c7f817fe
shuban-789 <cybergeek11929@gmail.com>
2024-09-21 22:07:49 -0700

    init_db.py for new database initialization

---
+++ Backend/init_db.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user=os.getenv("MYSQL_USR"),
+    password=os.getenv("MYSQL_PASS"),
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE coordinates (filename VARCHAR(255), latitude VARCHAR(255), longitude VARCHAR(255))")
